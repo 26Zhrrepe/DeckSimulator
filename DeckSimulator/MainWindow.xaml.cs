529bf199875425038a0fa5b8f2a25c16efd91099
26Zhrrepe <hisukasu1109@gmail.com>
2025-04-16 21:18:27 +0900

    Revert "とりあえずの形を作ったよ"
    This reverts commit 9c2c2e425118e503e89ee89d54c8b62b43ca7860.

--- DeckSimulator/MainWindow.xaml.cs
+++ DeckSimulator/MainWindow.xaml.cs
@@ -1,47 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace DeckSimulator
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> deck = new();
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void AddCard_Click(object sender, RoutedEventArgs e)
-        {
-            var cardName = CardNameTextBox.Text.Trim();
-            if (!string.IsNullOrEmpty(cardName))
-            {
-                deck.Add(cardName);
-                DeckListBox.Items.Add(cardName);
-                CardNameTextBox.Clear();
-            }
-        }
-
-        private void RunSimulation_Click(object sender, RoutedEventArgs e)
-        {
-            var random = new Random();
-            var hand = new List<string>();
-            var tempDeck = new List<string>(deck);
-
-            for (int i = 0; i < 5 && tempDeck.Count > 0; i++)
-            {
-                int index = random.Next(tempDeck.Count);
-                hand.Add(tempDeck[index]);
-                tempDeck.RemoveAt(index);
-            }
-
-            bool hasSnowpios = HasSnowpiosCheckBox.IsChecked == true;
-            bool conditionMet = !hasSnowpios || hand.Contains("スノーピオス");
-
-            ResultTextBox.Text = $"手札: {string.Join(", ", hand)}\n条件達成: {(conditionMet ? "はい" : "いいえ")}";
         }
     }
 }
